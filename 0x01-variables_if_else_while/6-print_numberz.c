99c5fcc5374812b437262c34d3708c05b669e01f
theodorechauke <chauketheodore01@gmail.com>
2023-06-16 16:35:36 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num==)
-		putchar((num % 10) + '0';
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
-	punchar('\n');
+	putchar('\n');
 
 	return (0);
 }

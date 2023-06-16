b3c53a2853e38bd73c1dc5121d9e932b1bc9890c
theodorechauke <chauketheodore01@gmail.com>
2023-06-16 16:28:54 -0700

    numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,19 +7,11 @@
  */
 int main(void)
 {
-int lowerCase = 'a';
-while (lowerCase <= 'z')
-{
-if (lowerCase == 'e' || lowerCase == 'q')
-{
-lowerCase += 1;
-}
-else
-{
-putchar(lowerCase);
-lowerCase += 1;
-}
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,19 @@
-#include <stdiio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Retrun: Always 0 (success)
+ * Retrun: 0 on success, non-zero on error.
  */
-
 int main(void)
 {
-int num;
-while (num < 10)
-{
-printf("%d", num);
-num += 1;
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num += 1;
+	}
+	printf("\n");
+	return (0);
 }
-printf("\n"0);
-return (0);

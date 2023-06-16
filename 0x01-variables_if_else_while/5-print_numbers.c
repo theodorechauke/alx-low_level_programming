3814e30ee2bffd8b5ae82b6d3f9c381f4bc9c000
theodorechauke <chauketheodore01@gmail.com>
2023-06-16 13:10:20 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdiio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retrun: Always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+while (num < 10)
+{
+printf("%d", num);
+num += 1;
+}
+printf("\n"0);
+return (0);

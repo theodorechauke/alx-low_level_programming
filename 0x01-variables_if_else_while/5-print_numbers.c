afe9cd7b7fd8a969b2f2b26af52b6858d982644a
theodorechauke <chauketheodore01@gmail.com>
2023-06-16 18:31:15 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
- * Retrun: 0 (Success)
+ * Retrun: Always 0 (Success)
  */
-int main(void)
-{
-	int num;
 
-	while (num < 10)
-	{
-		printf("%d", num);
-		num += 1;
-	}
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
 	printf("\n");
 	return (0);
 }

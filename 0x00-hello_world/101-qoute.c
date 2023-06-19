7b68fc9a322a73cb1d6b2ecb6e874ec9fce91591
theodorechauke <chauketheodore01@gmail.com>
2023-06-19 00:58:17 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unlstd.h>
+/**
+ * main - A c program that prints a line to the stadnard error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+return (1);
+}

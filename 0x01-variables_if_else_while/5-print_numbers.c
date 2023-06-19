6b5177243acf28c1d39dd4aec356cf06768aba78
theodorechauke <chauketheodore01@gmail.com>
2023-06-19 04:22:40 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,12 +11,12 @@
 int main(void)
 
 {
-	int i;
+	int a;
 
-	for (i = 0; i < 10; i++)
+	for (a = 0; a < 10; a++)
 	{
-	printf("%d", i);
+	printf("%d", a);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }

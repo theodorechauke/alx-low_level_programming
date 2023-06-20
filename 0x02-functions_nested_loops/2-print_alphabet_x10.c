62eb065adc997557fad958bf3d62eb50f88221fa
theodorechauke <chauketheodore01@gmail.com>
2023-06-20 11:44:07 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,10 +4,13 @@
  * print_alphabet_x10 -  Print 10 times the alphabet
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
+
 char ch;
 int i;
+i = 10;
 while (i < 10)
 {
 for (ch = 'a'; ch <= 'z'; ch++)

530ba045422678fbb761de46f41f9ebbaba63e38
theodorechauke <chauketheodore01@gmail.com>
2023-06-22 11:54:19 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 3 == 0) && (x % 1 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 		printf("FizzBuzz");
 		}
@@ -31,7 +31,7 @@ int main(void)
 		}
 		if (x != 100)
 		{
-			printf(" " );
+			printf(" ");
 		}
 		}
 		printf("\n");

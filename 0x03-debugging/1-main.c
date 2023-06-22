086060e3dc49a45c957e2cb3b3c41bd2934eef24
theodorechauke <chauketheodore01@gmail.com>
2023-06-22 15:45:53 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,20 +5,18 @@
 * Return: 0
 */
 
+
+
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-
-	while (i < 10)
-	{
-	putchar('0' + i);
-	i++;
-
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }

05247490f3dfce97c2fa43964ddf8a3aa8da0310
theodorechauke <chauketheodore01@gmail.com>
2023-06-26 15:41:07 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /**
- * main -Generates random valid passwords for the program 101-crackme
+ * main -Generates random valid passwords for the
+ * program 101-crackme
  * Return: Always 0.
  */
 
@@ -40,11 +41,13 @@ int main(void)
 				password[index] -= diff_half1;
 
 				break;
+
 			}
 		}
 		for (index = 0; password[index]; index++)
 		{
 			if (password[index] >= (33 + diff_half2))
+
 
 				password[index] -= diff_half2;
 				break;

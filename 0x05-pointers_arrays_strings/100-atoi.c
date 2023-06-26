d37bc530911bc6d115fd660db82b61ec5aa5d3f8
theodorechauke <chauketheodore01@gmail.com>
2023-06-26 15:49:14 -0700

    Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,14 @@ int _atoi(char *s)
 	int c = 0;
 
 	unsigned int ni = 0;
-	int main = 1;
+	int min = 1;
 	int isi = 0;
 
 	while (s[c])
 	{
 		if (s[c] == 45)
 		{
-			main *= -1;
+			min *= -1;
 		}
 		while (s[c] >= 48 && s[c] <= 57)
 		{
@@ -33,6 +33,6 @@ int _atoi(char *s)
 		}
 		c++;
 	}
-	ni *= main;
+	ni *= min;
 	return (ni);
 }

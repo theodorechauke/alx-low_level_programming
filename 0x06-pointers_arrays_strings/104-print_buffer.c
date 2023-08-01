a00b210f86fc4fa9cc868bc7ed71d134d9de8503
theodorechauke <chauketheodore01@gmail.com>
2023-08-01 03:32:36 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints buffer
- * @b: buffer
+ * print_buffer - prints buffer.
+ * @b: buffer.
  * @size: size
  * Return: void
  */
 
 void print_buffer(char *b, int size)
-
 {
 	int o, j, i;
 
@@ -23,32 +22,29 @@ void print_buffer(char *b, int size)
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
-		printf("%08x:", o);
+		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
 			else
-			printf(" ");
-
+				printf("  ");
 			if (i % 2)
 			{
 				printf(" ");
 			}
 		}
-
 		for (i = 0; i < j; i++)
 		{
-					int c = *(b + o + i);
+			int c = *(b + o + i);
 
-					if (c < 32 || c > 132)
-					{
-						c = '.';
-					}
-
-					printf("%c", c);
-				}
-				printf("\n");
-				o += 10;
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
 	}
 }
